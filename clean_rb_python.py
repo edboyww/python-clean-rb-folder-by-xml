8bb573433e1cc0b982fc31ad30d020ae38fb2463
edboyww <edboyww@gmail.com>
2024-12-31 12:50:28 +0100

    Add autocomplete for paths

--- clean_rb_python.py
+++ clean_rb_python.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from datetime import datetime
 from typing import List
 from urllib.parse import unquote
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import NestedCompleter
 
 # Parse the command line arguments
 
@@ -28,16 +30,16 @@ parser.add_argument(
     default=True,
 )
 parser.add_argument(
-    "--skip",
+    "--skip-folder",
     type=str,
     action="store",
     default=None,
-    help="skip the cleaning for these strings in the paths, divided by ',' (applies to both the local files and the paths in the XML file)",
+    help="skip the cleaning for these strings in the folder paths, divided by ',' (applies to both the local files and the paths in the XML file)",
 )
 parser.add_argument(
     "--include-streaming",
     action="store_true",
-    help="include stored streaming service file urls (they are skipped by adding 'tidal', 'soundcloud', 'beatport', 'itunes' to the skip list by default)",
+    help="include stored streaming service file urls to the cleaning process (they are skipped by adding 'tidal', 'soundcloud', 'beatport', 'itunes' to the skip list by default)",
     default=False,
 )
 parser.add_argument(
@@ -57,20 +59,29 @@ args = parser.parse_args()
 
 
 # Function to determine if a path should be skipped based on the skip list and the include_streaming flag
-def should_skip_path(path: str):
-    skips: List[str] = []
+def should_skip_path(path: Path | str):
     if not args.include_streaming:
+        skips: List[str] = []
         skips.append("tidal")
         skips.append("soundcloud")
         skips.append("beatport")
         skips.append("itunes")
-    if args.skip:
-        for s in args.skip.split(","):
-            skips.append(s.lower())
-    if skips:
-        for s in skips:
-            if s in path.lower():
+        for skip in skips:
+            if type(path) is str and skip in path.lower():
                 return True
+            elif type(path) is Path and skip in path.name.lower():
+                return True
+
+    if args.skip_folder:
+        skip_folders: List[str] = []
+        for s in args.skip_folder.split(","):
+            skip_folders.append(s.lower())
+        for skip in skip_folders:
+            if type(path) is str and skip in path.lower():
+                return True
+            elif type(path) is Path and skip in str(path.parent).lower():
+                return True
+
     return False
 
 
@@ -94,9 +105,7 @@ def get_path_list_from_rekordbox_xml():
     for track in xml_tracks:
         location = track.get("Location")
         if location and not should_skip_path(location):
-            xml_paths.append(
-                unquote(location.replace("file://localhost/", "").replace("/", os.sep))
-            )
+            xml_paths.append(unquote(location.replace("file://localhost/", "")))
 
     return xml_paths
 
@@ -115,38 +124,60 @@ def determine_common_path(paths: List[str]):
     return common_path
 
 
+# Build a dict of the folder structure for the autocomplete function
+type AutocompleteDict = dict[str, AutocompleteDict | None]
+
+
+def autocomplete_dict(path: Path) -> None | AutocompleteDict:
+    iter_dir = [item for item in path.iterdir() if item.is_dir()]
+    if len(iter_dir) == 0:
+        return None
+    else:
+        folder_dict: AutocompleteDict = {}
+        for item in iter_dir:
+            folder_dict[item.name + " /"] = autocomplete_dict(item)
+        return folder_dict
+
+
 # The main code starts here
 # -------------------------
 
 # Get the paths from the XML file and determine the root path for the cleaning process
 
 xml_paths = get_path_list_from_rekordbox_xml()
+initial_common_path = determine_common_path(xml_paths)
 
-common_path = determine_common_path(xml_paths)
 print(
-    f"\nThe common path in the XML file is:\n{common_path}\nDo you want to {"clean" if args.clean else "simulate cleaning"} this folder?\n\n(Y)es / (N)o"
+    "Set the folder you want to clean starting with the common folder for all the files in the XML file. Press TAB to see the folders on a current level and SPACE to get to the next level. You can set the path by pressing ENTER.\n"
 )
-inp: str = ""
+
+autocomplete_common_path = autocomplete_dict(Path(determine_common_path(xml_paths)))
+assert autocomplete_common_path is not None
+completer = NestedCompleter.from_nested_dict((autocomplete_common_path))
+
+common_path: str = ""
+
 while True:
+    input_path = (
+        prompt(initial_common_path, completer=completer).strip().replace(" ", "")
+    )
+
+    if not os.path.exists(input_path):
+        print("The path does not exist. Please enter a valid path.\n")
+        continue
+    elif initial_common_path not in input_path:
+        print(
+            "The path does not contain the common path of the XML file. Please enter a valid path.\n"
+        )
+
+    print("\nIs the path okay? (Y)es / (N)o\n")
     inp = input().lower()
     if inp not in ("y", "n"):
         print("Please enter 'Y' or 'N' to answer the question")
-    else:
+    elif inp == "y":
+        common_path = input_path
         break
-if inp == "n":
-    print("\nPlease enter the path you want to clean:")
-    while True:
-        input_path = input().strip().replace('"', "")
-        if not os.path.exists(input_path):
-            print("The path does not exist. Please enter a valid path.")
-        elif common_path not in input_path:
-            print(
-                "The path does not contain the common path of the XML file. Please enter a valid path."
-            )
-        else:
-            break
-    common_path = input_path
-inp = ""
+
 
 # Message before starting the cleaning process, and determine the file postfix for the results file
 
@@ -190,7 +221,7 @@ skipped_files = 0
 skipped_details = "\nSkipped files:\n--------------\n"
 for path in Path(common_path).rglob("*"):
     if path.is_file():
-        resolved_entry = f"{path.resolve()}"
+        resolved_entry = f"{path.as_posix()}"
         if should_skip_path(resolved_entry):
             skipped_files += 1
             skipped_details += f"\nS: {resolved_entry.replace(common_path, '')}"
@@ -198,7 +229,7 @@ for path in Path(common_path).rglob("*"):
             deleted_files += 1
             deleted_details += f"\nD: {resolved_entry.replace(common_path, '')}"
             if args.clean:
-                path.unlink()
+                path.unlink()  # THIS LINE DELETES THE FILE!
 
 
 results = f"\nSUMMARY:\n========\nDeleted files: {deleted_files}\nSkipped files: {skipped_files}\n"
@@ -210,7 +241,7 @@ xml_paths_not_found_details = (
     "\nPaths in the XML file not found:\n---------------------------------\n"
 )
 for xml_path in xml_paths:
-    if not os.path.exists(xml_path):
+    if xml_path in common_path and not os.path.exists(xml_path):
         xml_paths_not_found.append(xml_path)
 if len(xml_paths_not_found) > 0:
     for notfound in xml_paths_not_found:
